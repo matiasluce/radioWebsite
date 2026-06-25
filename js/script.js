3d453f3e3742426c90bab4a72d8aa668bdc756d4
Matu <matiaslucero1717@gmail.com>
2026-06-25 18:41:00 -0300

    proyecto base

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Esperamos a que todo el DOM de la página esté completamente cargado
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Estructura de la radio cargada correctamente.");
+    
+    // Aquí puedes incluir eventos personalizados o analíticas cuando expandas la web
+});
