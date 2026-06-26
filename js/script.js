5dff62789ec88dfe0520e3714168b0390ed42496
Matias Lucero <matiaslucero.dev@gmail.com>
2026-06-26 00:14:37 -0300

    elimino comentarios

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// Esperamos a que todo el DOM de la página esté completamente cargado
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Estructura de la radio cargada correctamente.");
-    
 });
